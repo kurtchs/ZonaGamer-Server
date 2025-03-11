a4b8224690f0fa8321ff174de531536356687e39
“Kurt <chaconsaavedrakurt@gmailcomgit config --global user.name Kurt Chacongit config --global user.email chaconsaavedrakurt@gmailcom>
2025-03-11 10:50:51 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const jsonServer = require("json-server")
+
+const server = jsonServer.create()
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+const allowAccesFromAnywhere = (req, res, next) => {
+res.header("Access-Control-Allow-Origin", "*")
+next()
+}
+
+server.use(allowAccesFromAnywhere)
+
+const router = jsonServer.router("db.json")
+
+server.use(router)
+
+const PORT = 5005
+
+server.listen(PORT, () => {
+    console.log("servidor andando")
+    console.log(`Ejecutandose localmente en http://localhost:${PORT}`)
+})
+
